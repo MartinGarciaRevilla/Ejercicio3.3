c2f24ed56ca8339767d4f5b5fd22f2f192cb9fcd
MartinGarciaRevilla <146463192+MartinGarciaRevilla@users.noreply.github.com>
2023-10-03 19:00:03 +0200

    Add files via upload

---
+++ proyecto/BasDeDatos.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS db;
+Create DATABASE db;
+USE db;
+
+CREATE TABLE Jugadores (
+ID INT NOT NULL,
+Username VARCHAR(60),
+Contraseña VARCHAR(60),
+DNI VARCHAR(9),
+
+PRIMARY KEY(ID)
+)ENGINE = InnoDB;
+
+CREATE TABLE Partidas (
+id int NOT NULL,
+Fecha varchar(60),
+Hora varchar(60),
+Duracion varchar(60),
+
+PRIMARY KEY(id)
+)ENGINE = InnoDB;
+
+Create Table Historial(
+idj INT NOT NULL,
+idp INT NOT NULL,
+FOREIGN KEY (idj) REFERENCES Jugadores(ID),
+FOREIGN KEY (idp) REFERENCES Partidas(id),
+)ENGINE = InnoDB;
+
+INSERT INTO Jugadores VALUES(1,'Alberto','Alberto1','12345678B');
+INSERT INTO Jugadores VALUES(2,'Martin','Martin1','22345678B');
+INSERT INTO Jugadores VALUES(3,'Arnau','Arnau1','22245678B');
+
+INSERT INTO Partidas VALUES(1,'15/02/2021','11:30','90 min');
+INSERT INTO Partidas VALUES(2,'15/02/2022','12:30','100 min');
+INSERT INTO Partidas VALUES(3,'15/02/2023','13:30','120 min');
+INSERT INTO Partidas VALUES(4,'15/02/2024','14:30','120 min');
+INSERT INTO Partidas VALUES(5,'15/02/2025','15:30','120 min');
+
+
+INSERT INTO Historial VALUES(1,5);
+INSERT INTO Historial VALUES(4,7);
+INSERT INTO Historial VALUES(3,2);
+
+
+SELECT Partidas.id FROM Jugadores, Partidas, Historal WHERE Jugadores.Username = 'Arnau' AND Partidas.Fecha = '15/02/2023' AND Jugadores.ID = Historial.idj AND Historial.idp = Partidas.id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
